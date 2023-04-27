a69bbfe4c9ccc19ae5bbb5c5968498fcf9e1a650
Oseias Costa <oseiascosta@Mac-Pro-de-Oseias.local>
2023-04-27 12:50:49 -0300

    create: headline component

---
+++ app/components/headline/Headline.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default function Headline() {
+  return (
+    <Container>
+        <Description>
+            <div className='headline__title'>
+            </div>
+        </Description>
+    </Container>
+  )
+}
+
+const Container = styled.section`
+
+`
+const Description = styled.div`
+
+`
